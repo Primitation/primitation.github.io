494a85c0468c8a25346627875fe9b765a85d4c91
Primitation <109825015+Primitation@users.noreply.github.com>
2024-02-29 01:36:40 +0100

    test

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ document.getElementById('downloadImage').addEventListener('click', function () {
     var tempLink = document.createElement('a');
     tempLink.href = 'https://drive.google.com/uc?id=YOUR_FILE_ID';
     tempLink.setAttribute('download', 'FileName');
+    // Open the link in a new tab or window
+    tempLink.target = '_blank';
 
     document.body.appendChild(tempLink);
     tempLink.click();
