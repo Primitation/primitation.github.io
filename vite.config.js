2b4c9b8379ce5780c03ef5771ce89546c41afaee
Primitation <109825015+Primitation@users.noreply.github.com>
2025-01-23 18:17:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     build: {
-        assetsInclude: ['**/*.glb'], // Include .glb files in the build process
+        assetsInclude: ['src/**/*.glb'], // Include .glb files in the build process
         emptyOutDir: true, // also necessary
     },
 }
